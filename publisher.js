17c100b913df96c75bb88f8bd8e13a8a79c5f608
Denisa Marcisovska <dmarcisovska@gmail.com>
2024-11-10 17:46:04 -0800

    installed axios, connected to quote API, grabbed quote body and author

--- publisher.js
+++ publisher.js
@@ -1,18 +1,36 @@
-const zmq = require("zeromq")
+import zmq from "zeromq";
+import axios from "axios";
 
-async function run() {
-  const sock = new zmq.Publisher()
-
-  await sock.bind("tcp://127.0.0.1:3000")
-  console.log("Publisher bound to port 3000")
-
-  while (true) {
-    console.log("sending a multipart message envelope")
-    await sock.send(["kitty cats", "meow!"])
-    await new Promise(resolve => {
-      setTimeout(resolve, 500)
-    })
+async function getQuote() {
+  try {
+    const response = await axios.get("https://favqs.com/api/qotd", {
+    });
+    const quote = response.data.quote;
+    const quote_body = response.data.quote.body
+    const quote_author = response.data.quote.author
+    return { quote_body, quote_author };
+  } catch (error) {
+    console.error("Error fetching quote:", error);
+    return "Something went wrong and a quote could not be send";
   }
 }
 
-run()
+async function run() {
+  const sock = new zmq.Publisher();
+
+  await sock.bind("tcp://127.0.0.1:3000");
+  console.log("Publisher bound to port 3000");
+
+  while (true) {
+    const { quote_body, quote_author } = await getQuote();
+    console.log("Sending daily quote body:", quote_body);
+    await sock.send(["quote_body", quote_body]);
+    console.log("Sending daily quote author:", quote_author);
+    await sock.send(["quote_author", quote_author]);
+    await new Promise((resolve) => {
+      setTimeout(resolve, 86400000); 
+    });
+  }
+}
+
+run();
